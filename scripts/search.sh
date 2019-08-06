a1d7087aee6f38df7836ed0d9f978b76a6e7353d
Yi-Wei Chen <feberium@gmail.com>
2019-08-06 15:43:29 -0500

    Update the requirements, exp and gitignore

---
+++ scripts/search.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+DATE=`date +%m%d`
+python train_search.py --data cifar10 --batchsz 64 --lr 0.1 --wd 0.0005 --gpu 1 --model_path exp --exp_path "exp/cifar10-${DATE}-" --seed 1 --train_portion 0.1
