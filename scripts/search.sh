76b17e8f0ad0bd76ebcfd63e9346879b63103021
Yi-Wei Chen <feberium@gmail.com>
2019-08-07 19:24:21 -0500

    Fix bug in the script

--- scripts/search.sh
+++ scripts/search.sh
@@ -8,11 +8,11 @@ DATE=`date +%m%d`
 
 #EXP_PATH="exp/${DATE}_cifar10_flip_noise_07_gpu"
 #python train_search.py --data cifar10 --batchsz 64 --lr 0.1 --wd 0.0005 --gpu 3 --model_path exp \
-#    --exp_path ${EXP_PATH} --seed 1 --train_portion 0.9 --dataset cifar10 --gold_fraction 1.0 \
+#    --exp_path ${EXP_PATH} --seed 1 --train_portion 0.9 --dataset cifar10 \
 #    --corruption_prob 0.7 --corruption_type flip --gold_fraction 0
 
 
 EXP_PATH="exp/${DATE}_cifar10_uniform_noise_07_gpu"
 python train_search.py --data cifar10 --batchsz 64 --lr 0.1 --wd 0.0005 --gpu 2 --model_path exp \
-    --exp_path ${EXP_PATH} --seed 1 --train_portion 0.9 --dataset cifar10 --gold_fraction 1.0 \
+    --exp_path ${EXP_PATH} --seed 1 --train_portion 0.9 --dataset cifar10 \
     --corruption_prob 0.7 --corruption_type unif --gold_fraction 0
