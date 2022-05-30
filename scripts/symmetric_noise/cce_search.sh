bdff2cf94e85c843a0ab8a6b5e899a554952dfc9
Yi-Wei Chen <feberium@gmail.com>
2022-05-30 12:15:57 -0500

    Update search scripts

--- scripts/symmetric_noise/cce_search.sh
+++ scripts/symmetric_noise/cce_search.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-DATE=`date +%m%d`
+DATE=`date +%m%d%y_%H%M%S`
 SEED=$1
 CORRUPTION_PROB=$2
 GPU=$3
 LOSS="cce"
-EXP_PATH="logs/${DATE}_cifar10_${LOSS}_symmetric-${CORRUPTION_PROB}_gpu"
+EXP_PATH="logs/${DATE}_seed-${SEED}_cifar10_${LOSS}_symmetric-${CORRUPTION_PROB}_gpu"
 
 python train_search.py --data cifar10 --batchsz 64 --gpu "${GPU}" --exp_path "${EXP_PATH}" --seed "${SEED}" \
     --train_portion 0.9 --dataset cifar10 --corruption_prob "${CORRUPTION_PROB}" --corruption_type unif \
