e4733952b569567f67a83e6d897d0ac7f68b934b
Yi-Wei Chen <feberium@gmail.com>
2022-06-06 16:04:15 -0500

    Make exp_path readable

--- scripts/symmetric_noise/retrain.sh
+++ scripts/symmetric_noise/retrain.sh
@@ -6,14 +6,15 @@ CORRUPTION_PROB=$2
 GPU=$3
 ARCH=$4
 LOSS="rll"
-EXP_PATH="logs/train/${DATE}_seed-${SEED}_cifar10_${ARCH}_gpu"
 
 if [[ "${ARCH}" =~ "resnet" ]]
 then
+  EXP_PATH="logs/train/${DATE}_seed-${SEED}_cifar10_${ARCH}_${LOSS}_symmetric-${CORRUPTION_PROB}_gpu"
   python train.py --data cifar10 --batchsz 96 --lr 0.025 --wd 3e-4 --gpu "${GPU}" --epochs 600 \
   --exp_path "${EXP_PATH}" --seed "${SEED}" --corruption_prob "${CORRUPTION_PROB}" --corruption_type unif \
   --gold_fraction 0 --loss_func "${LOSS}" --cutout --arch "${ARCH}"
 else
+  EXP_PATH="logs/train/${DATE}_seed-${SEED}_cifar10_${ARCH}_gpu"
   python train.py --data cifar10 --batchsz 96 --lr 0.025 --wd 3e-4 --gpu "${GPU}" --epochs 600 \
   --exp_path "${EXP_PATH}" --seed "${SEED}" --corruption_prob "${CORRUPTION_PROB}" --corruption_type unif \
   --gold_fraction 0 --loss_func "${LOSS}" --auxiliary --cutout --arch "${ARCH}"
